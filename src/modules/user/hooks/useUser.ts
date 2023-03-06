b85398b138d70fcfc21d83e2c7490530fdf7270e
juliossena <juliosouzasena@gmail.com>
2023-03-06 08:52:07 -0300

    feat: insert table in user screen

--- src/modules/user/hooks/useUser.ts
+++ src/modules/user/hooks/useUser.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { URL_USER_ALL } from '../../../shared/constants/urls';
 import { MethodsEnum } from '../../../shared/enums/methods.enum';
@@ -7,14 +7,28 @@ import { useUserReducer } from '../../../store/reducers/userReducer/useUserReduc
 
 export const useUser = () => {
   const { users, setUsers } = useUserReducer();
+  const [usersFiltered, setUsersFiltered] = useState(users);
   const { request, loading } = useRequests();
 
   useEffect(() => {
     request(URL_USER_ALL, MethodsEnum.GET, setUsers);
   }, []);
 
+  useEffect(() => {
+    setUsersFiltered(users);
+  }, [users]);
+
+  const handleOnChangeSearch = (value: string) => {
+    if (!value) {
+      setUsersFiltered([...users]);
+    } else {
+      setUsersFiltered([...users.filter((user) => user.name.includes(value))]);
+    }
+  };
+
   return {
-    users,
+    users: usersFiltered,
     loading,
+    handleOnChangeSearch,
   };
 };
--- src/modules/user/screens/User.tsx
+++ src/modules/user/screens/User.tsx
@@ -1,14 +1,88 @@
+import { Spin } from 'antd';
+import { Input } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+
+import Button from '../../../shared/components/buttons/button/Button';
 import Screen from '../../../shared/components/screen/Screen';
+import {
+  DisplayFlexJustifyBetween,
+  DisplayFlexJustifyCenter,
+} from '../../../shared/components/styles/display.styled';
+import { LimitedContainer } from '../../../shared/components/styles/limited.styled';
+import Table from '../../../shared/components/table/Table';
+import { insertMaskInCpf } from '../../../shared/functions/cpf';
+import { insertMaskInPhone } from '../../../shared/functions/phone';
+import { UserType } from '../../login/types/UserType';
 import { useUser } from '../hooks/useUser';
 
-const User = () => {
-  const { users } = useUser();
+const { Search } = Input;
 
-  console.log('users', users);
+const columns: ColumnsType<UserType> = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Nome',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Telefone',
+    dataIndex: 'phone',
+    key: 'phone',
+    render: (text) => <a>{insertMaskInPhone(text)}</a>,
+  },
+  {
+    title: 'CPF',
+    dataIndex: 'cpf',
+    key: 'cpf',
+    render: (text) => <a>{insertMaskInCpf(text)}</a>,
+  },
+];
+
+const User = () => {
+  const { users, loading, handleOnChangeSearch } = useUser();
 
   return (
-    <Screen>
-      <div>user</div>
+    <Screen
+      listBreadcrumb={[
+        {
+          name: 'HOME',
+        },
+        {
+          name: 'USUÁRIOS',
+        },
+      ]}
+    >
+      {loading ? (
+        <DisplayFlexJustifyCenter>
+          <Spin size="large" />
+        </DisplayFlexJustifyCenter>
+      ) : (
+        <>
+          <DisplayFlexJustifyBetween margin="0px 0px 16px 0px">
+            <LimitedContainer width={240}>
+              <Search placeholder="Buscar usuário" onSearch={handleOnChangeSearch} enterButton />
+            </LimitedContainer>
+            <LimitedContainer width={120}>
+              <Button type="primary" onClick={() => null}>
+                Inserir
+              </Button>
+            </LimitedContainer>
+          </DisplayFlexJustifyBetween>
+          <Table columns={columns} dataSource={users} />
+        </>
+      )}
     </Screen>
   );
 };
